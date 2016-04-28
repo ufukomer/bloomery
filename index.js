f97c9bbde50f3af2a97e64365d75376b46afcc06
ufukomer <ufukomer@gmail.com>
2016-04-29 00:11:32 +0300

    Import css and js files

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 import 'babel-polyfill';
+import React from 'react';
+import { render } from 'react-dom';
+import Root from './containers/Root';
+import configureStore from './store/configureStore';
+import $ from 'jquery';
+import jQuery from 'jquery';
+window.jQuery = jQuery;
+
+require('./assets/css/index.css');
+
+require('./semantic/dist/components/tab');
+require('./semantic/dist/components/popup');
+require('./semantic/dist/components/state');
+require('./semantic/dist/components/transition');
+
+const store = configureStore();
+
+$(document).ready(() => {
+  render(
+    <Root store={store} />,
+    document.getElementById('root')
+  );
+});
