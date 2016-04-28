365ca73061adaed04e389264314db76e3b7a4bca
ufukomer <ufukomer@gmail.com>
2016-04-28 23:40:32 +0300

    Saved queries

--- reducers/query.js
+++ reducers/query.js
@@ -1,21 +1,45 @@
 import {
-  QUERY_REQUEST,
-  QUERY_SUCCESS,
-  QUERY_FAILURE
+  QUERY_REQUEST, QUERY_SUCCESS,
+  QUERY_FAILURE, QUERY_SAVE,
+  QUERY_DELETE
 } from '../constants';
 
 function recentQuery(state = [], action) {
   switch (action.type) {
     case QUERY_SUCCESS:
+    case QUERY_FAILURE:
       return [
         ...state,
         {
           id: state.reduce((maxId, todo) =>
             Math.max(todo.id, maxId), -1) + 1,
           sql: action.sql,
-          date: action.receivedAt
+          date: action.receivedAt,
+          status: !action.error
         }
       ];
+    default:
+      return state;
+  }
+}
+
+function savedQuery(state = [], action) {
+  switch (action.type) {
+    case QUERY_SAVE:
+      return [
+        ...state,
+        {
+          id: state.reduce((maxId, todo) =>
+            Math.max(todo.id, maxId), -1) + 1,
+          title: action.title,
+          description: action.description,
+          sql: action.sql
+        }
+      ];
+    case QUERY_DELETE:
+      return state.filter(queryItem =>
+        queryItem.id !== action.id
+      );
     default:
       return state;
   }
@@ -25,7 +49,8 @@ export default function query(state = {
   isPending: false,
   result: [],
   lastQuery: '',
-  recentQueries: []
+  recentQueries: [],
+  savedQueries: []
 }, action) {
   switch (action.type) {
     case QUERY_REQUEST:
@@ -45,7 +70,14 @@ export default function query(state = {
       return {
         ...state,
         isPending: false,
-        error: action.error
+        lastQuery: action.sql,
+        error: action.error,
+        recentQueries: recentQuery(state.recentQueries, action)
+      };
+    case QUERY_SAVE:
+    case QUERY_DELETE:
+      return {
+        savedQueries: savedQuery(state.savedQueries, action)
       };
     default:
       return state;
