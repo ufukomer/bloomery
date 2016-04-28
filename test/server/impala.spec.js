6e41e56e142aefbe79f5fdce1f3b4f27d3c43a46
ufukomer <ufukomer@gmail.com>
2016-04-28 23:33:02 +0300

    Rest API test

---
+++ test/server/impala.spec.js
@@ -0,0 +1,38 @@
+import http from 'http';
+import expect from 'expect';
+import request from 'superagent';
+import app from '../../server/server';
+
+describe('impala rest api', () => {
+  const sql = 'select * from sample_07 limit 5';
+  const port = 3333;
+  const server = http.createServer(app);
+
+  before((done) => {
+    server.listen(port, (err) => {
+      if (err) {
+        done(err);
+      } else {
+        done();
+      }
+    });
+  });
+
+  after((done) => {
+    server.close();
+    done();
+  });
+
+  it('should send response', (done) => {
+    request.get(`http://localhost:${port}/api/impala/${sql}`)
+      .end((err, res) => {
+        if (err) {
+          expect(err.response.body.message).toExist();
+          expect(err.response.body.message).toBeA('string');
+        } else {
+          expect(res.body).toBeAn('object');
+        }
+        done();
+      });
+  });
+});
