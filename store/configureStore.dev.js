c16c7098bd6f2c8cf3391d72124013b6126a2d90
ufukomer <ufukomer@gmail.com>
2016-05-14 07:30:17 +0300

    Development config

---
+++ store/configureStore.dev.js
@@ -0,0 +1,40 @@
+import { reducer, createLoader, createMiddleware } from 'redux-storage';
+import createEngine from 'redux-storage-engine-localstorage';
+import { createStore, applyMiddleware, compose } from 'redux';
+import filter from 'redux-storage-decorator-filter';
+import DevTools from '../containers/DevTools';
+import thunkMiddleware from 'redux-thunk';
+import createLogger from 'redux-logger';
+import rootReducer from '../reducers';
+import * as types from '../constants';
+
+const includedActions = [
+  types.CONNECTION_SUCCESS,
+  types.QUERY_SUCCESS,
+  types.QUERY_SAVE,
+  types.QUERY_DELETE,
+  types.SAVE_TEXT
+];
+
+export default function configureStore(initialState) {
+  const engine = filter(
+    createEngine('bloomery'),
+    ['query', 'connection']
+  );
+  const store = createStore(
+    reducer(rootReducer),
+    compose(
+      applyMiddleware(
+        thunkMiddleware,
+        createLogger(),
+        createMiddleware(engine, [], includedActions)
+      ),
+      DevTools.instrument()
+    ),
+    initialState
+  );
+
+  createLoader(engine)(store);
+
+  return store;
+}
