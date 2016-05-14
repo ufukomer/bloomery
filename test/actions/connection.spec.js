26711a8fe2b97131d1f6a0215ddcb47d01a0d72a
ufukomer <ufukomer@gmail.com>
2016-05-14 07:33:19 +0300

    Connection state tests

---
+++ test/actions/connection.spec.js
@@ -0,0 +1,60 @@
+import http from 'http';
+import expect from 'expect';
+import thunk from 'redux-thunk';
+import configureMockStore from 'redux-mock-store';
+import * as actions from '../../actions/index';
+import * as types from '../../constants';
+import app from '../../server/server';
+
+const middlewares = [thunk];
+const mockStore = configureMockStore(middlewares);
+
+describe('impala connection actions', () => {
+  describe('connectImpala', () => {
+    const config = {
+      host: '127.0.0.1',
+      port: 21000
+    };
+    const port = 3333;
+    const url = `http://localhost:${port}`;
+    const server = http.createServer(app);
+
+    before((done) => {
+      server.listen(port, (err) => {
+        if (err) {
+          done(err);
+        } else {
+          done();
+        }
+      });
+    });
+
+    after((done) => {
+      server.close();
+      done();
+    });
+
+    it('should create CONNECTION_REQUEST and CONNECTION_SUCCESS or CONNECTION_FAILURE', (done) => {
+      const expectedAction = { type: types.CONNECTION_REQUEST };
+      const store = mockStore({});
+
+      store.dispatch(actions.connectImpala(config, url))
+        .then(() => {
+          const action = store.getActions();
+
+          if (action[1].error) {
+            expect(action[0]).toEqual(expectedAction);
+            expect(action[1].type).toEqual('CONNECTION_FAILURE');
+            expect(action[1].error).toBeA('object');
+          } else {
+            expect(action[0]).toEqual(expectedAction);
+            expect(action[1].type).toEqual('CONNECTION_SUCCESS');
+            expect(action[1].config).toBeAn('object');
+            expect(action[1].message).toBeA('string');
+          }
+        })
+        .then(done)
+        .catch(done);
+    });
+  });
+});
---
+++ test/reducers/connection.js
@@ -0,0 +1,58 @@
+import expect from 'expect';
+import reducer from '../../reducers/connection';
+import * as types from '../../constants';
+
+describe('connection reducer', () => {
+  it('should return the initial state', () => {
+    expect(
+      reducer(undefined, {})
+    ).toEqual({
+      isConnecting: false,
+      isConnected: false,
+      config: {
+        host: '127.0.0.1',
+        port: 21000
+      }
+    });
+  });
+
+  it('should handle CONNECTION_REQUEST', () => {
+    expect(
+      reducer({}, {
+        type: types.CONNECTION_REQUEST
+      })).toEqual({
+        isConnecting: true
+      }
+    );
+  });
+
+  it('should handle CONNECTION_SUCCESS', () => {
+    const message = 'Connection is established.';
+    const config = {
+      host: '192.168.0.3',
+      port: 21000
+    };
+    expect(
+      reducer({}, {
+        type: types.CONNECTION_SUCCESS,
+        config,
+        message
+      })).toEqual({
+        isConnecting: false,
+        isConnected: true,
+        config,
+        message
+      }
+    );
+  });
+
+  it('should handle CONNECTION_FAILURE', () => {
+    expect(
+      reducer({}, {
+        type: types.CONNECTION_FAILURE
+      })).toEqual({
+        isConnecting: false
+      }
+    );
+  });
+});
